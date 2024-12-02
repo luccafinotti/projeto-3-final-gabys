a88b332def4263acc2fb0eee2966200a2511546a
luccafinotti <00001113174043sp@al.educacao.sp.gov.br>
2024-12-02 16:48:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro o príncipe?',
+    'O autor Maquiavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os cinco maiores países em extensão territorial?',
+    'Rússia, Canadá, China, Estados Unidos e Brasil.'
+)
+
+criaCartao(
+    'Matemática',
+    'Se um pizza é dividida em 8 fatias iguais e você come 3, qual fração da pizza ainda resta?',
+    'Restam 5 de 8.'
+)
+
+criaCartao(
+    'Ciências',
+    'Quais são as três camadas principais da Terra?',
+    'As três camadas principais da Terra são a crosta, o manto e o núcleo.'
+)
